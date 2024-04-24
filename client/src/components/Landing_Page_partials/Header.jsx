74f48662afd9b80171711041de54cd7ee9f2e9ca
Jay Mishra <jaymishra0305@gmail.com>
2024-04-25 01:05:56 +0530

    Landing Page UI changes

--- client/src/components/Landing_Page_partials/Header.jsx
+++ client/src/components/Landing_Page_partials/Header.jsx
@@ -1,32 +1,98 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-function Header() {
-    const router = useRouter();
+const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const links = [
+        {
+            id: 1,
+            link: "/admin/auth",
+            text: "Event Manager"
+        },
+        {
+            id: 2,
+            link: "#",
+            text: "Learn More"
+        },
+        {
+            id: 3,
+            link: "#",
+            text: "About"
+        },
+        {
+            id: 4,
+            link: "/users/signup",
+            text: "Register"
+        },
+        {
+            id: 5,
+            link: "/users/signin",
+            text: "Log In"
+        },
+    ];
 
     return (
-        <header className="absolute w-full z-30">
-            <div className="max-w-6xl mx-auto px-4 sm:px-6">
-                <div className="flex items-center justify-between h-20">
-                    {/* Desktop navigation */}
-                    <nav className="flex grow">
-                        {/* Desktop sign in links */}
-                        <ul className="flex grow justify-end flex-wrap items-center">
-                            <li>
-                                <a
-                                    onClick={() =>
-                                        router.push("/admin/auth")
-                                    }
-                                    className="btn-sm text-white bg-[color:var(--darker-secondary-color)] hover:bg-[color:var(--secondary-color)] ml-3 cursor-pointer"
-                                >
-                                    Event Manager
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+        <div style={{ height: 74, backgroundColor: "#f05454", zIndex: -1 }} className="flex justify-between items-center w-full h-20 px-4 text-white fixed nav">
+            <div style={{ display: "flex" }}>
+                <img
+                    src="/favicon/favicon.ico"
+                    width={500}
+                    height={500}
+                    style={{ marginLeft: 95 }}
+                    alt="Logo"
+                    className="h-8 w-8"
+                />        <h1 style={{ fontFamily: "Courier New, Courier, monospace" }} className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 text-white duration-200 link-underline">
+                    <a
+                        className="link-underline link-underline-black"
+                        style={{
+                            fontFamily: "Courier New, Courier, monospace", fontSize: 20, padding: 10, marginTop: 10,
+                            color: "white", fontWeight: 700,
+                        }}
+                        href="http://localhost:3000/"
+                        // target="_blank"
+                        rel="noreferrer"
+                    >
+                        CampusConnect
+                    </a>
+                </h1>
             </div>
-        </header>
-    );
-}
 
-export default Header;
+            <ul className="hidden md:flex">
+                {links.map(({ id, link, text }) => (
+                    <li
+                        key={id}
+                        className="nav-links px-4 cursor-pointer capitalize font-medium text-white-500 hover:scale-105 hover:text-black duration-200 link-underline"
+                    >
+                        <Link href={link}>{text}</Link>
+                    </li>
+                ))}
+            </ul>
+
+            <div
+                onClick={() => setNav(!nav)}
+                className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+            >
+                {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+            </div>
+
+            {nav && (
+                <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+                    {links.map(({ id, link }) => (
+                        <li
+                            key={id}
+                            className="px-4 cursor-pointer capitalize py-6 text-4xl"
+                        >
+                            <Link onClick={() => setNav(!nav)} href={link}>
+                                {link}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
