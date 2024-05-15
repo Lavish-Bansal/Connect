097d5faf01466036991672de720c90609e36fbb6
Jay Mishra <jaymishra0305@gmail.com>
2024-05-15 22:48:34 +0530

    Added styling for Event Manager login page

--- client/src/pages/admin/auth.jsx
+++ client/src/pages/admin/auth.jsx
@@ -67,17 +67,23 @@ export default function signin({ adminIdCookie }) {
   };
 
   return (
-    <div className="m-2">
+    <div
+      style={{
+        backgroundColor: "beige",
+        backgroundImage: `url('/img/frontImg.jpeg')`,
+        height: "100vh",
+        // backgroundRepeat: "no-repeat",
+        height: "100vh"
+      }}
+    >
       <FiArrowLeft
         onClick={() => router.push("/")}
         size={24}
         className="cursor-pointer"
+        style={{ marginLeft: 10, height: 35, width: 40, color: "red" }}
       />
-      <div className="text-center text-3xl font-bold">
-        Admin Authentication Page
-      </div>
 
-      <div className="max-w-3xl mx-auto mt-10">
+      <div className="max-w-3xl mx-auto mt-10" style={{ width: "35vw" }}>
         <div className="flex items-center justify-center"></div>
 
         {message.errorMsg && (
@@ -92,61 +98,68 @@ export default function signin({ adminIdCookie }) {
           </h1>
         )}
 
-        <div className="bg-white p-5 rounded-lg mt-2">
-          {step === 1 && (
-            <form onSubmit={handleSubmit}>
-              <label className="block mb-2 text-sm font-medium text-gray-700">
-                Enter your Registered Email address
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                className="bg-gray-100 p-2 mx-2 mb-4 focus:outline-none rounded-lg w-full"
-                onChange={(e) => setEmail(e.target.value)}
-              />
+        <div style={{ display: "flex", justifyContent: "center", }}>
+          <div className="bg-white p-5 rounded-lg mt-2" style={{ height: "83vh", width: "28vw" }}>
+            <div className="text-center text-2xl font-bold">
+              Admin Authentication Page
+            </div>
+            {step === 1 && (
+              <form onSubmit={handleSubmit}>
+                <label style={{ marginLeft: 10, marginBottom: 18, marginTop: 30, font: "icon", color: "black" }} className="block mb-2 text-sm font-medium text-gray-700">
+                  Enter your Registered Email address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  className="bg-gray-100 p-2 mx-2 mb-4 focus:outline-none rounded-lg w-full"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
 
-              <label className="block mb-2 text-sm font-medium text-gray-700">
-                Enter your Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={password}
-                className="bg-gray-100 p-2 mx-2 mb-4 focus:outline-none rounded-lg w-full"
-                onChange={(e) => setPassword(e.target.value)}
-              />
+                <label style={{marginLeft: 10, font: "icon"}} className="block mb-2 text-sm font-medium text-gray-700">
+                  Enter your Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={password}
+                  className="bg-gray-100 p-2 mx-2 mb-4 focus:outline-none rounded-lg"
+                  style={{ width: "24vw" }}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
 
-              <button
-                type="submit"
-                className="btn text-white bg-[color:var(--darker-secondary-color)] hover:bg-[color:var(--secondary-color)] w-full mt-4 mb-4 sm:w-auto sm:mb-0"
-              >
-                Verify
-              </button>
-            </form>
-          )}
-          {step === 2 && (
-            <div>
-              <div className="bg-green-50 border-b border-green-400 text-green-800 text-sm p-4 flex justify-between">
-                <div>
-                  <div className="flex items-center">
-                    <p>
-                      <span className="font-bold">Hey there! </span>
-                      Welcome back, you re successfully signed in!
-                    </p>
+                <button
+                  type="submit"
+                  className="btn text-white bg-[color:var(--darker-secondary-color)] hover:bg-[color:var(--secondary-color)] w-full mt-4 mb-4 sm:w-auto sm:mb-0"
+                  style={{ marginLeft: 10, marginTop: 14 }}
+                >
+                  Verify
+                </button>
+              </form>
+            )}
+            {step === 2 && (
+              <div>
+                <div className="bg-green-50 border-b border-green-400 text-green-800 text-sm p-4 flex justify-between">
+                  <div>
+                    <div className="flex items-center">
+                      <p>
+                        <span className="font-bold">Hey there! </span>
+                        Welcome back, you re successfully signed in!
+                      </p>
+                    </div>
                   </div>
                 </div>
+                <button
+                  onClick={() => router.push("/admin/dashboard")}
+                  className="mt-4 bg-[color:var(--darker-secondary-color)] text-white py-2 px-4 rounded hover:bg-[color:var(--secondary-color)] transition ease-in-out"
+                >
+                  Go to your dashboard
+                </button>
               </div>
-              <button
-                onClick={() => router.push("/admin/dashboard")}
-                className="mt-4 bg-[color:var(--darker-secondary-color)] text-white py-2 px-4 rounded hover:bg-[color:var(--secondary-color)] transition ease-in-out"
-              >
-                Go to your dashboard
-              </button>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
